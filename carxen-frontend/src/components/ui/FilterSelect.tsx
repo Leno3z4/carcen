14c8985a9cf56b242c2111ceaab95954aefad3fd
Leno3z4 <leno3z4rrrri@gmail.com>
2026-07-24 21:12:28 +0100

    Add FilterSelect component for select inputs

--- carxen-frontend/src/components/ui/FilterSelect.tsx
+++ carxen-frontend/src/components/ui/FilterSelect.tsx
@@ -1,1 +1,7 @@
-.
+import { SelectHTMLAttributes } from "react";
+
+type FilterSelectProps = SelectHTMLAttributes<HTMLSelectElement>;
+
+export default function FilterSelect(props: FilterSelectProps) {
+  return <select {...props} />;
+}
