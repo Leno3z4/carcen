0e656a8c2f75dc6bb4c79f1b6c53c099bbccfed0
Leno3z4 <leno3z4rrrri@gmail.com>
2026-07-24 20:44:37 +0100

    Add Providers component for wrapping children

--- carxen-frontend/src/app/providers.tsx
+++ carxen-frontend/src/app/providers.tsx
@@ -1,1 +1,9 @@
-.
+import { ReactNode } from "react";
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+export default function Providers({ children }: ProvidersProps) {
+  return <>{children}</>;
+}
