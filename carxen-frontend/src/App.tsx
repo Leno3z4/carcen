eed143e8ada9a1d085dffcc317da63313f38af90
Leno3z4 <leno3z4rrrri@gmail.com>
2026-07-24 20:42:33 +0100

    Add routing for main application pages

--- carxen-frontend/src/App.tsx
+++ carxen-frontend/src/App.tsx
@@ -1,1 +1,21 @@
-.
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import MarketDetailsPage from "./pages/MarketDetails";
+import PortfolioPage from "./pages/Portfolio";
+import ProfilePage from "./pages/Profile";
+import CreatorPage from "./pages/Creator";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/markets/:marketId" element={<MarketDetailsPage />} />
+      <Route path="/portfolio" element={<PortfolioPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/creator/:creatorId" element={<CreatorPage />} />
+    </Routes>
+  );
+}
+
+export default App;
