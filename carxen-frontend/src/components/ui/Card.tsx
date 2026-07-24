00feb421469ebd4401fed7260e6440daa2f52e50
Leno3z4 <leno3z4rrrri@gmail.com>
2026-07-24 21:47:51 +0100

    Refactor Card component to accept additional props

--- carxen-frontend/src/components/ui/Card.tsx
+++ carxen-frontend/src/components/ui/Card.tsx
@@ -1,9 +1,22 @@
-import { ReactNode } from "react";
+import { HTMLAttributes } from "react";
+import clsx from "clsx";
 
-type CardProps = {
-  children: ReactNode;
-};
+type CardProps = HTMLAttributes<HTMLDivElement>;
 
-export default function Card({ children }: CardProps) {
-  return <>{children}</>;
+export default function Card({
+  className,
+  children,
+  ...props
+}: CardProps) {
+  return (
+    <div
+      className={clsx(
+        "rounded-3xl border border-neutral-200 bg-white p-6 shadow-sm",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 }
