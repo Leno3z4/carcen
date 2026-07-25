0d19a1a7eb4c187c1df8bb9a73bc02acaa16db59
Leno3z4 <leno3z4rrrri@gmail.com>
2026-07-25 11:39:10 +0100

    Add properties to Creator interface

--- carxen-frontend/src/types/creator.ts
+++ carxen-frontend/src/types/creator.ts
@@ -1,1 +1,8 @@
-export interface Creator {}
+export interface Creator {
+  id: string;
+  name: string;
+  platform: "YouTube" | "X";
+  followers: number;
+  activeMarkets: number;
+  avatar: string;
+}
