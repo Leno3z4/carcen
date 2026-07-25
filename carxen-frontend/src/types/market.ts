03b2fea36455ce85a8a3474b69812de6f72e079a
Leno3z4 <leno3z4rrrri@gmail.com>
2026-07-25 11:38:45 +0100

    Define Market interface with detailed properties

--- carxen-frontend/src/types/market.ts
+++ carxen-frontend/src/types/market.ts
@@ -1,1 +1,12 @@
-export interface Market {}
+export interface Market {
+  id: string;
+  creator: string;
+  platform: "YouTube" | "X";
+  question: string;
+  current: number;
+  target: number;
+  yes: number;
+  no: number;
+  volume: number;
+  endTime: number;
+}
